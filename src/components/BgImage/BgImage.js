69542910cf0e375dd4d53980217d22da65dc61bc
GKuzin13 <yo.is.josh@gmail.com>
2021-12-08 00:50:19 +0200

    Added a new background picture

--- src/components/BgImage/BgImage.js
+++ src/components/BgImage/BgImage.js
@@ -3,14 +3,14 @@ import styled from 'styled-components';
 export const BgImage = styled.div`
   background: linear-gradient(
       90deg,
-      rgba(0, 0, 0, 0.2) 0%,
-      rgba(0, 0, 0, 0.1) 100%
+      rgba(0, 0, 0, 0.3) 0%,
+      rgba(0, 0, 0, 0.15) 100%
     ),
     url(${(props) => props.img});
   background-size: cover;
   background-repeat: no-repeat;
   background-position: center;
-  position: absolute;
+  position: fixed;
   top: 0;
   width: 100%;
   min-height: 100%;
