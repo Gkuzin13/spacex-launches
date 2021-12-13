7e9817e8434cb8bf58ae28119193b4bf8be6017e
GKuzin13 <yo.is.josh@gmail.com>
2021-12-13 09:54:52 +0200

    Changed page bottom padding

--- src/pages/History/History.styled.js
+++ src/pages/History/History.styled.js
@@ -1,17 +1,5 @@
 import styled from 'styled-components';
 
-export const Wrapper = styled.div``;
-
-export const GaugeWrapper = styled.div`
-  display: grid;
-  place-items: center;
-  grid-template-columns: repeat(2, 1fr);
-  grid-template-rows: repeat(4, 1fr);
-  gap: 24px;
-
-  @media (min-width: ${({ theme }) => theme.mobile}) {
-    grid-template-columns: repeat(4, 1fr);
-    height: 32rem;
-    padding: 12rem 20rem;
-  }
+export const Wrapper = styled.div`
+  padding-bottom: 5rem;
 `;
