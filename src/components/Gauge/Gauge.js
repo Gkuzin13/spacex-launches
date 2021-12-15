f0e698da3b5faf9c61ea838d6b97f4893ddda418
GKuzin13 <yo.is.josh@gmail.com>
2021-12-15 17:06:31 +0200

    added gauge count up animation

--- src/components/Gauge/Gauge.js
+++ src/components/Gauge/Gauge.js
@@ -1,9 +1,40 @@
 import { Container, Digit } from './Gauge.style';
 import calcOffset from '../../utils/calcOffset';
+import { useEffect, useState } from 'react';
 
 const Gauge = ({ stats, text, midText, percentage }) => {
-  const rate = Math.round((stats.success / stats.total) * 100);
-  const offset = calcOffset(rate).toString();
+  const [successCount, setSuccessCount] = useState(0);
+  const [totalCount, setTotalCount] = useState(0);
+  const [percentCount, setPercentCount] = useState(0);
+
+  const percent = Math.round((stats.success / stats.total) * 100);
+  const offset = calcOffset(percent).toString();
+
+  useEffect(() => {
+    const successCounter = setInterval(() => {
+      const newCount = successCount + 1;
+      setSuccessCount(newCount);
+    }, 5);
+    const totalCounter = setInterval(() => {
+      const newCount2 = totalCount + 1;
+      setTotalCount(newCount2);
+    }, 2);
+    const percentCounter = setInterval(() => {
+      const newCount3 = percentCount + 1;
+      setPercentCount(newCount3);
+    }, 10);
+
+    if (successCount >= stats.success || percentage)
+      clearInterval(successCounter);
+    if (totalCount >= stats.total || percentage) clearInterval(totalCounter);
+    if (percentCount >= percent || !percentage) clearInterval(percentCounter);
+
+    return () => {
+      clearInterval(successCounter);
+      clearInterval(totalCounter);
+      clearInterval(percentCounter);
+    };
+  }, [successCount, totalCount, percentCount, stats, percent, percentage]);
 
   if (!stats) {
     return <></>;
@@ -11,7 +42,6 @@ const Gauge = ({ stats, text, midText, percentage }) => {
 
   return (
     <Container offset={offset}>
-      <div></div>
       <svg
         xmlns='http://www.w3.org/2000/svg'
         xmlnsXlink='http://www.w3.org/1999/xlink'
@@ -22,7 +52,7 @@ const Gauge = ({ stats, text, midText, percentage }) => {
           style={{ fill: 'none' }}
         />
         <path
-          className='purple'
+          className='blue'
           d='M 60 95 A 45 45 90 1 1 120 95'
           style={{ fill: 'none' }}
         />
@@ -30,11 +60,11 @@ const Gauge = ({ stats, text, midText, percentage }) => {
           <tspan x='90%' dy='-5%'>
             {text}
           </tspan>
-          <tspan x='90%' dy='10%'>
+          <tspan x='90%' dy='10.5%'>
             {midText}
           </tspan>
-          <Digit x='90%' dy='20%'>
-            {percentage ? rate + `%` : stats.success + `/` + stats.total}
+          <Digit x='90%' dy='20.5%'>
+            {percentage ? percentCount + `%` : successCount + `/` + totalCount}
           </Digit>
         </text>
       </svg>
