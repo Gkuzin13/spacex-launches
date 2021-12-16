5f88587ec784b6795bdede417b312dcaacdba443
GKuzin13 <yo.is.josh@gmail.com>
2021-12-17 00:39:00 +0200

    added timer aria role attribute

--- src/components/Timer/Timer.js
+++ src/components/Timer/Timer.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import { DigitContainer, Container, Digit } from './Timer.styled';
-import { calcTimeLeft } from '../../utils/calcTimeLeft';
+import calcTimeLeft from '../../utils/calcTimeLeft';
 
 const Timer = ({ date }) => {
   const [timeLeft, setTimeLeft] = useState(calcTimeLeft(date));
@@ -14,26 +14,24 @@ const Timer = ({ date }) => {
   });
 
   return (
-    <Container>
-      <div>
-        <time dateTime={date} />
-        <DigitContainer>
-          <span>DAYS</span>
-          <Digit>{timeLeft.days}</Digit>
-        </DigitContainer>
-        <DigitContainer>
-          <span>HOURS</span>
-          <Digit>{timeLeft.hours}</Digit>
-        </DigitContainer>
-        <DigitContainer>
-          <span>MINUTES</span>
-          <Digit>{timeLeft.minutes}</Digit>
-        </DigitContainer>
-        <DigitContainer>
-          <span>SECONDS</span>
-          <Digit>{timeLeft.seconds}</Digit>
-        </DigitContainer>
-      </div>
+    <Container role='timer'>
+      <time dateTime={date} />
+      <DigitContainer>
+        <span>DAYS</span>
+        <Digit>{timeLeft.days}</Digit>
+      </DigitContainer>
+      <DigitContainer>
+        <span>HOURS</span>
+        <Digit>{timeLeft.hours}</Digit>
+      </DigitContainer>
+      <DigitContainer>
+        <span>MINUTES</span>
+        <Digit>{timeLeft.minutes}</Digit>
+      </DigitContainer>
+      <DigitContainer>
+        <span>SECONDS</span>
+        <Digit>{timeLeft.seconds}</Digit>
+      </DigitContainer>
     </Container>
   );
 };
