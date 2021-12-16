2d5c76bf0c681fe75f3c1827ccdcb8c67a2ad8fa
GKuzin13 <yo.is.josh@gmail.com>
2021-12-17 00:35:30 +0200

    added condition if value is less than 0

--- src/components/ArrowDown/ArrowDown.js
+++ src/components/ArrowDown/ArrowDown.js
@@ -4,10 +4,10 @@ import useScrollPosition from '../../hooks/useScrollPosition';
 
 const ArrowDown = ({ height }) => {
   const { scrollY } = useScrollPosition();
-  const opacity = 1 - (scrollY / height / 0.8).toFixed(2) || 0;
+  const opacity = 1 - (scrollY / height / 0.85).toFixed(2);
 
   return (
-    <ArrowWrapper opacity={opacity}>
+    <ArrowWrapper opacity={opacity > 0 ? opacity : 0}>
       <DownOutlined />
     </ArrowWrapper>
   );
